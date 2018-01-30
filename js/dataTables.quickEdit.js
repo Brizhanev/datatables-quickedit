352156ffdbd139f730acf8d2918a64bafb0fcce6
Oleg Brizhanev <mr.brizhanev@yandex.ru>
2018-01-30 23:43:01 +0300

    Отформатировал код

--- js/dataTables.quickEdit.js
+++ js/dataTables.quickEdit.js
@@ -13,130 +13,130 @@
  *
  */
 
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				root = window;
-			}
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery', 'datatables.net'], function ($) {
+            return factory($, window, document);
+        });
+    }
+    else if (typeof exports === 'object') {
+        // CommonJS
+        module.exports = function (root, $) {
+            if (!root) {
+                root = window;
+            }
 
-			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net')(root, $).$;
-			}
+            if (!$ || !$.fn.dataTable) {
+                $ = require('datatables.net')(root, $).$;
+            }
 
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
+            return factory($, root, root.document);
+        };
+    }
+    else {
+        // Browser
+        factory(jQuery, window, document);
+    }
+}(function ($, window, document, undefined) {
+    'use strict';
 
-var DataTable = $.fn.dataTable;
+    var DataTable = $.fn.dataTable;
 
-DataTable.quickEdit = {};
+    DataTable.quickEdit = {};
 
-DataTable.quickEdit.version = '1.0.0';
+    DataTable.quickEdit.version = '1.0.0';
 
-DataTable.quickEdit.init = function ( dt ) {
+    DataTable.quickEdit.init = function (dt) {
 
-	var ctx = dt.settings()[0];
-	var init = ctx.oInit.quickEdit;
-	var defaults = DataTable.defaults.quickEdit;
-	var opts = init === undefined ?
-		defaults :
-		init;
+        var ctx = dt.settings()[0];
+        var init = ctx.oInit.quickEdit;
+        var defaults = DataTable.defaults.quickEdit;
+        var opts = init === undefined ?
+            defaults :
+            init;
 
-	// Set defaults
-	var selector = 'td';
-	var callback = function() {};
-	var editableClass = 'quickEditable';
+        // Set defaults
+        var selector = 'td';
+        var callback = function () {
+        };
+        var editableClass = 'quickEditable';
 
-	ctx._quickEdit = {};
-	
-	if ( $.isPlainObject( opts ) ) {
-		if ( opts.selector !== undefined ) {
-			selector = opts.selector;
-		}
-		if ( opts.callback !== undefined ) {
-			callback = opts.callback;
-		}
-		if ( opts.editableClass !== undefined ) {
-			editableClass = opts.editableClass;
-		}
-	}
+        ctx._quickEdit = {};
 
-	dt.iterator( 'table', function ( ctx ) {
+        if ($.isPlainObject(opts)) {
+            if (opts.selector !== undefined) {
+                selector = opts.selector;
+            }
+            if (opts.callback !== undefined) {
+                callback = opts.callback;
+            }
+            if (opts.editableClass !== undefined) {
+                editableClass = opts.editableClass;
+            }
+        }
 
-		var that = this;
+        dt.iterator('table', function (ctx) {
 
-		ctx._quickEdit.selector = selector;
-		ctx._quickEdit.callback = callback;
-		ctx._quickEdit.editableClass = editableClass;
+            var that = this;
 
-		this.on('dblclick', selector, function () {
-			$(this).attr('contenteditable', 'true');
-			$(this).addClass(editableClass);
-			$(this).focus();
-		});
-	
-		this.on('blur', selector, function () {
-			endEdit(this, editableClass, callback, that);
-		});
-	
-		this.on('keyup', selector, function (event) {
-			if (event.which == 13) {
-				endEdit(this, editableClass, callback, that);
-			}
-		});
-		
-	} );
+            ctx._quickEdit.selector = selector;
+            ctx._quickEdit.callback = callback;
+            ctx._quickEdit.editableClass = editableClass;
 
-};
+            this.on('dblclick', selector, function () {
+                $(this).attr('contenteditable', 'true');
+                $(this).addClass(editableClass);
+                $(this).focus();
+            });
 
-function endEdit(cell, editableClass, callback, table) {
+            this.on('blur', selector, function () {
+                endEdit(this, editableClass, callback, that);
+            });
 
-	$(cell).attr('contenteditable', 'false');
-	$(cell).removeClass(editableClass);
+            this.on('keyup', selector, function (event) {
+                if (event.which == 13) {
+                    endEdit(this, editableClass, callback, that);
+                }
+            });
 
-	var tableCell = table.cell(cell);
+        });
 
-	tableCell.data($(cell).text());
+    };
 
-	var tableRecord = table.data()[tableCell.index().row];
+    function endEdit(cell, editableClass, callback, table) {
 
-	callback(tableRecord, table);
+        $(cell).attr('contenteditable', 'false');
+        $(cell).removeClass(editableClass);
 
-}
+        var tableCell = table.cell(cell);
 
-var apiRegister = DataTable.Api.register;
-var apiRegisterPlural = DataTable.Api.registerPlural;
+        tableCell.data($(cell).text());
 
-apiRegister( 'quickEdit()', function () {
-	return this.iterator( 'table', function ( ctx ) {
-		DataTable.quickEdit.init( new DataTable.Api( ctx ) );
-	} );
-} );
+        var tableRecord = table.data()[tableCell.index().row];
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Initialisation
- */
-$(document).on('preInit.dt.dtQuickEdit', function (e, ctx) {
-	if ( e.namespace !== 'dt' ) {
-		return;
-	}
-	DataTable.quickEdit.init( new DataTable.Api( ctx ) );
-} );
+        callback(tableRecord, table);
 
-return DataTable.quickEdit;
+    }
+
+    var apiRegister = DataTable.Api.register;
+
+    apiRegister('quickEdit()', function () {
+        return this.iterator('table', function (ctx) {
+            DataTable.quickEdit.init(new DataTable.Api(ctx));
+        });
+    });
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     * Initialisation
+     */
+    $(document).on('preInit.dt.dtQuickEdit', function (e, ctx) {
+        if (e.namespace !== 'dt') {
+            return;
+        }
+        DataTable.quickEdit.init(new DataTable.Api(ctx));
+    });
+
+    return DataTable.quickEdit;
 
 }));
